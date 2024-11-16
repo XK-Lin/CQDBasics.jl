754323b6c4ae0e08f83ecc93a72a8c5a0bc999bc
Xukun Lin <linxk123@gmail.com>
2024-11-15 21:10:43 -0800

    updated examples

--- examples/CQDDataAnalysis.jl
+++ examples/CQDDataAnalysis.jl
@@ -1,9 +1,16 @@
 using CQDBasics
 
 cwd = pwd()
+subfolers = Dict(
+    "Alex 156" => ["13"],
+    "Alex 165" => ["59"],
+    "Alex 396" => ["1"],
+    "FS Low" => ["1"],
+    "FS High" => ["1"]
+)
 for dir ∈ filter(isdir, readdir(cwd, join=true))
     categorize_folders(dir, ("θₙ", "Branching Condition", "Average Method", "Initial μₙ [relative to local B_ext]", "Sigmoid Field", "Bₙ Bₑ Ratio"))
     sort_folders(dir)
     get_subfolder_summary_tables(dir)
 end
-clean_folders(cwd, true)
+clean_folders(cwd, false, union(values(subfolers)...))
